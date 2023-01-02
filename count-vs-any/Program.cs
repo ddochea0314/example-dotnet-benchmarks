189d4d7f466f432e730a24b82a28a6da5f194ba4
ddochea0314 <ddochea0314@gmail.com>
2023-01-02 22:33:37 +0900

    초기 커밋.

---
+++ count-vs-any/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+Console.WriteLine("Hello, World!");
+
+BenchmarkRunner.Run<CountVsAny>();
+
+
+[MemoryDiagnoser]
+public class CountVsAny
+{
+	public int[] items;
+	public CountVsAny()
+	{
+        items = Enumerable.Range(0, 1000).ToArray();
+    }
+
+    [Benchmark]
+    public bool IsExistsUseCount()
+	{
+		return items.Count() != 0;
+    }
+
+    [Benchmark]
+    public bool IsExistsUseAny()
+	{
+        return items.Any();
+    }
+
+    [Benchmark]
+    public bool IsItem100ExistsCount()
+	{
+        return items.Count(x => x == 100) != 0;
+    }
+
+    [Benchmark]
+	public bool IsItem100ExistsAny()
+	{
+        return items.Any(x => x == 100);
+    }
+}
