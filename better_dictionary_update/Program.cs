5f057b688950c14a56da8d55e8c5aadb3327eda3
ddochea <ddochea0314@gmail.com>
2023-01-24 15:16:51 +0900

    Dictionary Update 벤치마크 사례 추가

---
+++ better_dictionary_update/Program.cs
@@ -0,0 +1,88 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+BenchmarkRunner.Run<BetterDictionaryUpdate>();
+
+public class BetterDictionaryUpdate
+{
+    public readonly Guid _id;
+    public readonly Dictionary<Guid, UserClass> _clsUser;
+    public readonly Dictionary<Guid, UserStruct> _structUser;
+
+    public BetterDictionaryUpdate()
+    {
+        _id = Guid.NewGuid();
+        _clsUser = new()
+        {
+            { _id, new UserClass() { ID = _id, Age = 10, Name = "John" } }
+        };
+        _structUser = new()
+        {
+            { _id, new UserStruct() { ID = _id, Age = 10, Name = "John" } }
+        };
+    }
+
+    [Benchmark]
+    public UserClass UpdateUserClass()
+    {
+        if(_clsUser.TryGetValue(_id, out var user))
+        {
+            user.Name = "Tom";
+            return user;
+        }
+        return default!;
+    }
+
+    [Benchmark]
+    public UserClass UpdateUserClass_Unsafe()
+    {
+        ref var user = ref CollectionsMarshal.GetValueRefOrNullRef(_clsUser, _id);
+
+        if(!Unsafe.IsNullRef(ref user))
+        {
+            user.Name = "Tom";
+            return user;
+        }
+        return default!;
+    }
+
+    [Benchmark]
+    public UserStruct UpdateUserStruct()
+    {
+        if (_structUser.TryGetValue(_id, out var user))
+        {
+            user.Name = "Tom";
+            _structUser[_id] = user; // struct는 값 형식이므로 재 대입해야 한다.
+            return user;
+        }
+        return default;
+    }
+
+    [Benchmark]
+    public UserStruct UpdateUserStruct_Unsafe()
+    {
+        ref var user = ref CollectionsMarshal.GetValueRefOrNullRef(_structUser, _id);
+        if(!Unsafe.IsNullRef(ref user))
+        {
+            user.Name = "Tom";
+            return user;
+        }
+        return default;
+    }
+}
+public class UserClass
+{
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+
+public struct UserStruct
+{
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ better_dictionary_updateTests/BetterDictionaryUpdateTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass()]
+    public class BetterDictionaryUpdateTests
+    {
+
+        [TestMethod()]
+        public void UpdateUserClassTest()
+        {
+            var obj = new BetterDictionaryUpdate();
+            obj.UpdateUserClass();
+            Assert.IsTrue(obj._clsUser.GetValueOrDefault(obj._id)?.Name == "Tom");
+        }
+
+        [TestMethod()]
+        public void UpdateUserClass_UnsafeTest()
+        {
+            var obj = new BetterDictionaryUpdate();
+            obj.UpdateUserClass_Unsafe();
+            Assert.IsTrue(obj._clsUser.GetValueOrDefault(obj._id)?.Name == "Tom");
+        }
+
+        [TestMethod()]
+        public void UpdateUserStructTest()
+        {
+            var obj = new BetterDictionaryUpdate();
+            obj.UpdateUserStruct();
+            Assert.IsTrue(obj._structUser.GetValueOrDefault(obj._id).Name == "Tom");
+        }
+
+        [TestMethod()]
+        public void UpdateUserStruct_UnsafeTest()
+        {
+            var obj = new BetterDictionaryUpdate();
+            obj.UpdateUserStruct_Unsafe();
+            Assert.IsTrue(obj._structUser.GetValueOrDefault(obj._id).Name == "Tom");
+        }
+    }
+}
