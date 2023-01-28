73916187756a3544c815e12cc7f5171ea4bdbe35
ddochea <ddochea0314@gmail.com>
2023-01-28 12:21:47 +0900

    프로젝트 추가

---
+++ src/better_stopwatch_create/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System.Diagnostics;
+
+BenchmarkRunner.Run<BetterStopwatchCreate>();
+
+
+[MemoryDiagnoser]
+[SimpleJob(runtimeMoniker: RuntimeMoniker.Net60)]
+[SimpleJob(runtimeMoniker: RuntimeMoniker.Net70)]
+public class BetterStopwatchCreate
+{
+    public async ValueTask<long> CreateStopWatch_UsingNewKeywoard()
+    {
+        var watch = new Stopwatch();
+        watch.Start();
+        await Task.Delay(100);
+        watch.Stop();
+        return watch.ElapsedMilliseconds;
+    }
+
+    [Benchmark]
+    public async Task Use_NewKeywoard()
+    {
+        foreach (var _ in Enumerable.Range(0, 10))
+        {
+            await CreateStopWatch_UsingNewKeywoard();
+        }
+    }
+    
+    public async ValueTask<long> CreateStopWatch_StartNew()
+    {
+        var watch = Stopwatch.StartNew();
+        await Task.Delay(100);
+        watch.Stop();
+        return watch.ElapsedMilliseconds;
+    }
+
+    [Benchmark]
+    public async Task Use_StartNew()
+    {
+        foreach (var _ in Enumerable.Range(0, 10))
+        {
+            await CreateStopWatch_StartNew();
+        }
+    }
+
+}
---
+++ tests/better_stopwatch_createTests/BetterStopwatchCreateTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class BetterStopwatchCreateTests
+    {
+        [TestMethod()]
+        public async Task CreateStopWatch_UsingNewKeywoardTest()
+        {
+            var obj = new BetterStopwatchCreate();
+            var actual = await obj.CreateStopWatch_UsingNewKeywoard();
+            Assert.AreEqual(100, actual, delta: 10.0, message: $"{actual}");
+        }
+
+        [TestMethod()]
+        public async Task CreateStopWatch_StartNewTest()
+        {
+            var obj = new BetterStopwatchCreate();
+            var actual = await obj.CreateStopWatch_StartNew();
+            Assert.AreEqual(100, actual, delta: 10.0, message: $"{actual}");
+        }
+    }
+}
